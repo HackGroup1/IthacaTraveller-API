b1810d0e02da53f2d5c9ac9ae00c8a9a4f15b8f3
Frank Dai <sd924@cornell.edu>
2023-11-18 18:29:41 -0500

    modified:   src/db.py

--- src/db.py
+++ src/db.py
@@ -2,35 +2,72 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-assoc_course_instructors = db.Table(
-    "association_course_instructors",
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("instructor_id", db.Integer, db.ForeignKey("user.id"))
+assoc_features_locations = db.Table(
+    "association_features_locations",
+    db.Column("feature_id", db.Integer, db.ForeignKey("feature.id")),
+    db.Column("location_id", db.Integer, db.ForeignKey("location.id"))
 )
 
-assoc_course_students = db.Table(
-    "association_course_students",
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("student_id", db.Integer, db.ForeignKey("user.id"))
+assoc_posts_users_likes = db.Table(
+    "association_posts_users_likes",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("post_id", db.Integer, db.ForeignKey("post.id"))
 )
 
-class Spot(db.Model):
+class Feature(db.Model):
     """
-    Spot Model
+    Feature Model
     """
-    __tablename__ = "spot"
+
+    __tablename__ = "feature"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    locations = db.relationship("Location", secondary = assoc_features_locations, back_populates = "features")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a feature object
+        """
+
+        self.name = kwargs.get("name", "")
+
+    def serialize(self):
+        """
+        Serialize a feature object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "locations": [location.id for location in self.locations]
+        }
+    
+    def simple_serialize(self):
+        """
+        Serialize a feature object
+        """
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Location(db.Model):
+    """
+    Location Model
+    """
+    __tablename__ = "location"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     longitude = db.Column(db.Double, nullable = False)
     latitude = db.Column(db.Double, nullable = False)
     name = db.Column(db.String, nullable = False)
     description = db.Column(db.String, nullable = False)
-    # image_name = db.Column(db.String, nullable = False)
     feature = db.Column(db.String, nullable = False)
     posts = db.relationship("Post", cascade = "delete")
+    features = db.relationship("Feature", secondary = assoc_features_locations, back_populates = "locations")
 
     def __init__(self, **kwargs):
         """
-        Initialize a spot object
+        Initialize a location object
         """
 
         self.longitude = kwargs.get("longitude", 0.0)
@@ -49,7 +86,8 @@ class Spot(db.Model):
             "latitude": self.latitude,
             "name": self.name,
             "description": self.description,
-            "posts": [post.serialize() for post in self.posts]
+            "posts": [post.serialize() for post in self.posts],
+            "features": [feature.simple_serialize() for feature in self.features]
         }
 
     def simple_serialize(self):
@@ -61,7 +99,7 @@ class Spot(db.Model):
             "longitude": self.longitude,
             "latitude": self.latitude,
             "name": self.name,
-            "description": self.description,
+            "description": self.description
         }
     
 class Post(db.Model):
@@ -71,16 +109,18 @@ class Post(db.Model):
     __tablename__ = "post"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     comment = db.Column(db.String, nullable = False)
+    location_id = db.Column(db.Integer, db.ForeignKey("location.id"), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
-    likes = db.relationship("Course", secondary = assoc_course_students, back_populates = "students")
+    likes = db.relationship("User", secondary = assoc_posts_users_likes, back_populates = "users")
 
     def __init__(self, **kwargs):
         """
         Initialize a post object
         """
 
-        self.netid = kwargs.get("netid", "")
-        self.name = kwargs.get("name", "")
+        self.comment = kwargs.get("comment", "")
+        self.location_id = kwargs.get("name", "")
+        self.user_id = kwargs.get("user_id", "")
 
     def serialize(self):
         """
@@ -107,20 +147,21 @@ class User(db.Model):
     """
     User Model
     """
+
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String, nullable = False)
-    netid = db.Column(db.String, nullable = False)
-    instructing = db.relationship("Course", secondary = assoc_course_instructors, back_populates = "instructors")
-    studying = db.relationship("Course", secondary = assoc_course_students, back_populates = "students")
+    username = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    posts = db.relationship("Post", cascade = "delete")
+    posts_liked = db.relationship("Post", secondary = assoc_posts_users_likes, back_populates = "likes")
 
     def __init__(self, **kwargs):
         """
         Initialize a user object
         """
 
-        self.netid = kwargs.get("netid", "")
-        self.name = kwargs.get("name", "")
+        self.username = kwargs.get("username", "")
+        self.password = kwargs.get("", "")
 
     def serialize(self):
         """
