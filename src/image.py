fb5a73b8e9187bcca87bf3893e952f6b614b1684
Huajie <hz642@cornell.edu>
2023-11-19 20:34:14 -0500

    added user routes

--- src/image.py
+++ src/image.py
@@ -31,8 +31,9 @@ def image_route(app):
     def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+    #------------Post route-----------------------------------------------------
     @app.route('/api/images/posts/<int:post_id>/', methods=['POST'])
-    def upload(post_id):
+    def upload_post_image(post_id):
         post = Post.query.filter_by(id=post_id).first()
 
         if post is None:
@@ -62,7 +63,7 @@ def image_route(app):
 
 
     @app.route('/api/images/posts/<int:post_id>/')
-    def get_image(post_id):
+    def get_post_image(post_id):
        post = Post.query.filter_by(id=post_id).first()
        
        if post is None:
@@ -74,7 +75,7 @@ def image_route(app):
        return failure_response("file not found")
     
     @app.route('/api/images/post/<int:post_id>', methods=["DELETE"])
-    def delete_image(post_id):
+    def delete_post_image(post_id):
         post = Post.query.filter_by(id=post_id).first()
 
         if post is None:
@@ -90,4 +91,66 @@ def image_route(app):
             check = check_existance('IMAGE_FOLDER_POST', post_id)
 
         return success_response("image removed")
-            
+    
+    #------------------------user route---------------------------------------
+    @app.route('/api/images/user/<int:user_id>/', methods=['POST'])
+    def upload_user_image(user_id):
+        user = User.query.filter_by(id=user_id).first()
+
+        if user is None:
+            return failure_response("user not found")
+
+        if 'file' not in request.files:
+            return failure_response("file keyword not provided", 400)
+    
+        file = request.files['file']
+
+        if file is None:
+            return failure_response("file not uploaded", 400)
+        
+        if not allowed_file(file.filename):
+            return failure_response("file not supported", 400)
+        
+        check = check_existance('IMAGE_FOLDER_USER', user_id)
+
+        while check is not None:
+            os.remove(check)
+            check = check_existance('IMAGE_FOLDER_USER', user_id)
+
+        folder_path = app.config['IMAGE_FOLDER_USER']
+        filename = os.path.join(folder_path, f"{user_id}.{file.filename.rsplit('.', 1)[1].lower()}")
+        file.save(filename)
+        return success_response("images successfully saved at server", 201)
+
+
+    @app.route('/api/images/users/<int:user_id>/')
+    def get_user_image(user_id):
+       user = User.query.filter_by(id=user_id).first()
+       
+       if user is None:
+            return failure_response("user not found")
+       
+       check = check_existance('IMAGE_FOLDER_USER', user_id)
+       if check is not None:
+            return send_file(check, mimetype='image/jpeg')
+       return failure_response("file not found")
+    
+    @app.route('/api/images/user/<int:user_id>', methods=["DELETE"])
+    def delete_user_image(user_id):
+        user = User.query.filter_by(id=user_id).first()
+
+        if user is None:
+            return failure_response("user not found")
+        
+        check = check_existance('IMAGE_FOLDER_USER', user_id)
+
+        if check is None: 
+            return failure_response("image not found")
+        
+        while check is not None:
+            os.remove(check)
+            check = check_existance('IMAGE_FOLDER_USER', user_id)
+
+        return success_response("image removed")
+    
+
