76f9ec66dfcd90f04c82a5dcacc9d17f755cb442
Frank Dai <sd924@cornell.edu>
2023-11-28 20:29:23 -0500

    /.

--- src/app.py
+++ src/app.py
@@ -526,13 +526,13 @@ def verify_user():
     
 
     if user.password == hashed_password:
-         res = {
-             "verify":True,
-             "user_id": user.serialize().get("id")
-             }
+        res = {
+            "verify":True,
+            "user_id": user.serialize().get("id")
+            }
         return success_response(res)
     else:
-         res = {"verify":False}
+        res = {"verify":False}
         return success_response(res, 405)
 
 
