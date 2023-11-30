590e11a5e48bf0920753ab8bc31ea4547acc5ceb
Frank Dai <sd924@cornell.edu>
2023-11-29 21:48:44 -0500

    Forbidden creating two features with same name

--- src/app.py
+++ src/app.py
@@ -86,6 +86,11 @@ def add_feature():
     if name is None:
         return failure_response("missing parameter", 400)
 
+    feature = Feature.query.filter_by(name = name).first()
+
+    if feature is not None:
+        return failure_response("feature already exists", 400)
+
     feature = Feature(name = name)
 
     db.session.add(feature)
